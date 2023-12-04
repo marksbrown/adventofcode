658e71f6e5071f920820feeff40339c0d04135fa
Mark S. Brown <sandcalc@gmail.com>
2023-12-04 15:15:52 +0000

    day 1 complete

--- day1.py
+++ day1.py
@@ -1,18 +1,61 @@
 from useful import load_data_gen
-from string import ascii_lowercase
+from string import ascii_lowercase, digits
+from warnings import warn
+from random import random
+from time import sleep
 
-testing = False
-test_data = ["1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]
 fn = "day1"
+testing = False
+
+written_digits = (
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+)
+
+def parse(v):
+    """
+    >>> parse("1abc2")
+    '12'
+    >>> parse("two1nine")
+    '219'
+    >>> parse("eightwothree")
+    '823'
+    >>> parse("abcone2threexyz")
+    '123'
+    >>> parse("treb7uchet")
+    '7'
+    >>> parse("4nineeightseven2")
+    '49872'
+    >>> parse("ftjjqbgphtmhthreesix1six")
+    '3616'
+    """
+    values = []
+    for j, sym in enumerate(v):
+        if sym in digits:
+            values.append(sym)
+        for digit in written_digits:
+            if digit in v[j : j + len(digit)]:
+                values.append(str(written_digits.index(digit)))
+
+    return "".join(values)
+
 
 if testing:
-    data = test_data
-else:
-    data = load_data_gen(fn)
+    import doctest
+    doctest.testmod()
 
 result = 0
-for r in data:
-    values = list(filter(lambda x :x not in ascii_lowercase, r))
+for r in load_data_gen(fn, testing):
+    values = parse(r)
     value = int(values[0] + values[-1])
     result += int(value)
 
--- useful.py
+++ useful.py
@@ -1,7 +1,12 @@
 from os.path import join
 
-def load_data_gen(fn):
-    with open(join("data", fn), 'r') as f:
+
+def load_data_gen(fn, testing=False):
+    if testing:
+        x = open("test_data", "r")
+    else:
+        x = open(join("data", fn), "r")
+    with x as f:
         for row in f:
-            row = row.strip('\n')
+            row = row.strip("\n")
             yield row
