5ed114b5724448007bd541b8a44d4e2bb5877b90
Mark S. Brown <sandcalc@gmail.com>
2023-12-05 18:44:41 +0000

    day 2 complete

--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
 from useful import load_data_gen
+from itertools import product
 
 
 def parse_game(game: str):
@@ -15,6 +16,53 @@ def parse_game(game: str):
             yield data
 
     return num, list(f(data.split(";")))
+
+
+def total(game):
+    """
+    totals of all colours used in a game
+    >>> total([{"blue" : 3, "red" : 4}, {"red" : 1, "blue" : 2}, {"green" : 1}])
+    {'blue': 5, 'red': 5, 'green': 1}
+    """
+    result = {}
+    for r in game:
+        for col in r:
+            if col not in result:
+                result[col] = r[col]
+            else:
+                result[col] += r[col]
+
+    return result
+
+
+def min_cols(game):
+    """
+    Find minimum number of each colour needed to complete this game
+    >>> min_cols([{"blue" : 3, "red" : 4}, {"red" : 1, "blue" : 2}, {"green" : 1}])
+    {'blue': 3, 'red': 4, 'green': 1}
+    """
+    result = {}
+    for r in game:
+        for col in r:
+            if col not in result:
+                result[col] = r[col]
+            elif r[col] > result[col]:
+                result[col] = r[col]
+
+    return result
+
+
+def power(r):
+    """
+    Multiply all colours together
+    >>> power({'blue': 6, 'red': 4, 'green': 2})
+    48
+    """
+    t = 1
+    for k in r:
+        t *= r[k]
+
+    return t
 
 
 testing = False
@@ -34,12 +82,21 @@ def valid_game(games) -> bool:
     return True
 
 
+if testing:
+    import doctest
+
+    doctest.testmod()
+
 total = 0
+power_total = 0
 for row in load_data_gen(fn, testing):
     game_id, data = parse_game(row)
 
-    print(game_id, row, data, valid_game(data), sep="\n", end="\n\n")
+    min_balls = min_cols(data)
+    t = power(min_balls)
+    print(f"{min_balls} gives {t}")
+    power_total += t
     if valid_game(data):
-        print(game_id, data)
         total += game_id
 print(f"Total of game_ids is {total}")
+print(f"The total of power applied to min balls is {power_total}")
