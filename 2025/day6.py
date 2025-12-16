a298de63d2326a5688de9d5bbf2d036b751e879d
Mark S. Brown <sandcalc@gmail.com>
2025-12-16 21:37:44 +0000

    day six complete. This took a painfully long time

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,6 +1,7 @@
 import re
+from itertools import pairwise
 from functools import reduce
-from numpy import transpose
+from numpy import array, rot90
 
 testing = False
 
@@ -16,21 +17,56 @@ op = {
     "/": lambda x, y: x / y,
 }
 
-lines = []
-operations = []
 with open(fn) as f:
     for line in f:
         line = line.strip("\n")
-        line = [t for t in re.split("\s+", line) if t]
+
+spacings = [
+    0,
+]
+operations = []
+for j, elem in enumerate(line):
+    if elem in op:
+        operations.append(elem)
+        if j:
+            spacings.append(j)
+else:
+    spacings.append(len(line) + 1)
+
+lines = []
+row = {}
+with open(fn) as f:
+    for line in f:
+        line = line.replace("\n", "0")
         if line[0] in op.keys():
-            operations = line
-        else:
-            line = list(map(int, line))
-            lines.append(line)
+            break
+        for z, (l, r) in enumerate(pairwise(spacings)):
+            # print(line[l:r], end="|")
+            seq = line[l : r - 1]  # .replace(' ', '0')
+            seq = seq.strip("\s")
+            print(seq, end="|")
+            if not seq:
+                seq = [
+                    0,
+                ]
+            if z not in row:
+                row[z] = [
+                    list(seq),
+                ]
+            else:
+                row[z].append(list(seq))
+            z += 1
+        lines.append(row)
+        print("")
+print(operations)
+print("===")
 
-lines = transpose(lines)
+lines = []
+for line in row:
+    arr = array(row[line])
+    arr = rot90(arr)
+    lines.append(list(map(lambda x: int("".join(x)), arr)))
 
-# part a
 total = 0
 for j, line in enumerate(lines):
     print(line, operations[j], "=", end=" ")
